c30522d4332f0df594cf5fdda721944d42e26b5c
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-09-16 23:37:59 +0500

    todo in progress

---
+++ IntroductionToBlazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace IntroductionToBlazor.Components.Pages
+{
+	public partial class Factorial
+	{
+		int n = 0;
+		long factorial = 1;
+		void Calculate()
+		{
+			factorial = 1;
+			for (long i = 1; i <= n; i++)
+			{
+				factorial *= i;
+			}
+			// return factorial;
+		}
+	}
+}
