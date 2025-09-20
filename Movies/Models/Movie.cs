03a4f6544c2a067e29e719fe725f7a0d559b581b
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-09-20 23:27:32 +0500

    Movies DONE

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -5,12 +5,29 @@ namespace Movies.Models
 {
 	public class Movie
 	{
-		public int Id { get; set; }	
+		public int Id { get; set; }
+		
+		/// ///////////////////////////////////////////////
+		
+		[Required]//обязательное поле
+		[StringLength(60,MinimumLength =3)]
 		public string? Title { get; set; }
+		/// ///////////////////////////////////////////////
+
+		[RangeAttribute(typeof(DateOnly),"10-14-1888","12-31-2032")]
 		public DateOnly ReleaseDate { get; set; }
+		/// ///////////////////////////////////////////////
+
+		[Required]
+		[StringLength(30)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
 		public string? Genre { get; set; }
+		/// ///////////////////////////////////////////////
+
+		[Range(0,200)]
 		[DataType(DataType.Currency)]
 		[Column(TypeName ="decimal(18,2)")]
 		public decimal Price { get; set; }
+		/// ///////////////////////////////////////////////
 	}
 }
